30463c17f513ab7c97ac795dd83087e2a64ef4de
NIKHIL11 <nikhilchalla511@gmail.com>
2024-06-13 12:23:58 +0530

    commit1

--- 1.js
+++ 1.js
@@ -8,7 +8,7 @@ const empName="nikhil";
 var salary=50000;
 var age=20;
 
-var a=10;
+var a=5;
 var b=2;
 
 var result=a**b
@@ -16,6 +16,8 @@ console.log(`result of exponent is ${result}`)
 
 var result= a>b ? "a is big" : "b is big"
 console.log(result)
+
+
 
 
 
